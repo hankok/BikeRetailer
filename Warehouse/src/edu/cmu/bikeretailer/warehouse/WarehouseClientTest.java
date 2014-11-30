445930688faf935b76850adf68ac79368b4491b2
Satish <satishr@sase.ssn.edu.in>
2014-11-30 00:49:38 -0500

    Added executables

--- Warehouse/src/edu/cmu/bikeretailer/warehouse/WarehouseClientTest.java
+++ Warehouse/src/edu/cmu/bikeretailer/warehouse/WarehouseClientTest.java
@@ -4,6 +4,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Date;
 
 import edu.cmu.bikeretailer.api.WarehouseInterface;
 
@@ -16,12 +17,16 @@ public class WarehouseClientTest {
 	 */
 	public static void main(String[] args) throws RemoteException, NotBoundException {
 		// TODO Auto-generated method stub
-		Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+		Registry registry = LocateRegistry.getRegistry("ec2-54-69-71-197.us-west-2.compute.amazonaws.com", 1099);
 		WarehouseInterface warehouseApi = (WarehouseInterface) registry.lookup(WarehouseInterface.class.getSimpleName());
-		System.out.println(warehouseApi.getBikeInventory());
-		System.out.println(warehouseApi.updateInventory("1", 2));
-		System.out.println(warehouseApi.getBikeInventory());
-		System.out.println(warehouseApi.updateInventory("1", 3));
+		long t1 = new Date().getTime();
+		warehouseApi.getBikeInventory();
+		long t2 = new Date().getTime();
+		System.out.println(t2-t1);
+		//System.out.println(warehouseApi.getBikeInventory());
+		//System.out.println(warehouseApi.updateInventory("1", 2));
+		//System.out.println(warehouseApi.getBikeInventory());
+		//System.out.println(warehouseApi.updateInventory("1", 3));
 	}
 
 }
